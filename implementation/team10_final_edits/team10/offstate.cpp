7a69a129e5ea7029b277298fda56c5454c17196f
mariamartiyanova <122643825+mariamartiyanova@users.noreply.github.com>
2023-04-20 21:02:20 -0400

    Update offstate.cpp

--- implementation/team10_final_edits/team10/offstate.cpp
+++ implementation/team10_final_edits/team10/offstate.cpp
@@ -2,10 +2,6 @@
 
 OffState::OffState(QString items, QString title) : State (items, title) {}
 
-void OffState::printSelection(QString selectedItem){
-    qInfo() << "you selected = " << selectedItem;
-}
-//state button press returns
 int OffState::handlePressPowerButton(){return 0;}
 int OffState::handlePressMenuButton(){return -1;}
 int OffState::handlePressBackButton(){return -1;}
