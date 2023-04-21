c7afd169048985ffe21c396029bf9f46286172f1
mariamartiyanova <122643825+mariamartiyanova@users.noreply.github.com>
2023-04-20 20:22:44 -0400

    Update sessionsummarystate.h

--- implementation/team10_final_edits/team10/sessionsummarystate.h
+++ implementation/team10_final_edits/team10/sessionsummarystate.h
@@ -9,23 +9,23 @@
 class SessionSummaryState: public State
 {
 public:
-    SessionSummaryState(QString items, QString title);
-    void printSelection(QString selectedItem);
 
-    //state button press returns
+    SessionSummaryState(QString items, QString title);
+
     int handlePressPowerButton();
     int handlePressMenuButton();
     int handlePressBackButton();
-    int handlePressLeftButton();
-    int handlePressRightButton();
     int handlePressSelectorButton();
 
     void setDelete(bool enable);
     int setCurrentIndex(int i);
-    int getCurrentIndex();//return the index of the viewed session
+    int getCurrentIndex();
+    
 private:
+
     bool deleteOption;
     int currentIndex;
+    
 };
 
 #endif // SESSIONSUMMARYSTATE_H
