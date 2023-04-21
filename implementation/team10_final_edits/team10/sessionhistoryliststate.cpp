983a5f37701fd8f1d646f8ce29139dff2f391c7e
mariamartiyanova <122643825+mariamartiyanova@users.noreply.github.com>
2023-04-20 21:16:01 -0400

    Update sessionhistoryliststate.cpp

--- implementation/team10_final_edits/team10/sessionhistoryliststate.cpp
+++ implementation/team10_final_edits/team10/sessionhistoryliststate.cpp
@@ -2,7 +2,5 @@
 
 SessionHistoryListState::SessionHistoryListState(QString items, QString title) : State(items, title) {}
 
-int SessionHistoryListState::handlePressPowerButton(){return 1;}
-int SessionHistoryListState::handlePressMenuButton(){return 1;}
 int SessionHistoryListState::handlePressBackButton(){return 0;}
 int SessionHistoryListState::handlePressSelectorButton(){return 3;}
