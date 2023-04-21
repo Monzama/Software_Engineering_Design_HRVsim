f2adee0821a3e70aadfef9e9da8fde096bd2f6ad
mariamartiyanova <122643825+mariamartiyanova@users.noreply.github.com>
2023-04-20 20:53:13 -0400

    Update session.h

--- implementation/team10_final_edits/team10/session.h
+++ implementation/team10_final_edits/team10/session.h
@@ -2,40 +2,43 @@
 #define SESSION_H
 
 #include <QListWidget>
-class Session
-{
+
+class Session{
+    
 public:
+    
     Session(int id);
 
     ///may perform calculations for scores here, will remove the need for several set functions
-    bool update(int HR=-1, float coherence = -1,float achievScore = -1);//updates session data
+    bool update(int HR = -1, float coherence = -1,float achievScore = -1); //updates session data
 
+    QVector<float>* getHRData(); // returns the hr data list to be processed
+    QVector<float>* getTotalCoherence(); // returns coherence list top be processed
 
-    QVector<float>* getHRData();//returns the hr data list to be processed
-    QVector<float>* getTotalCoherence();//returns coherence list top be processed
+    float getAvgCoherence();
+    float getAchievScore();
+    float getCurrentCoherence();
 
-    float getAvgCoherence();//returns averageCoherence
-    float getAchievScore();//returns achievcementScore
-    float getCurrentCoherence();//returns most recent coherence data
-
-    int getLowTime();//returns lowTime
-    int getMedTime();//returns medTime
-    int getHighTime();//returns highTime
-    int getTime();//returns elapsedTimeInSeconds
-    int getID();//returns id
-    QString getDate();//returns session date
-    void timeIncrement(int level);//increments both lepsed time and level time
+    int getLowTime();
+    int getMedTime();
+    int getHighTime();
+    int getTime(); // returns elapsedTimeInSeconds
+    int getID();
+    QString getDate(); 
+    void timeIncrement(int level); // increments elapsed time and level time
 
 private:
+    
     int id;
-    int elapsedTimeInSeconds;//stores total session time
-    QVector<float> heartRateData;//stores the list of reported HR data every second
-    QVector<float> coherences;//stores list of coherence data every 5-seconds
-    float averageCoherence;//as it says
-    int lowTime, medTime, highTime; //time in seconds, at each coherence level
-    float achievementScore;//as it says
-    float currentCoherence;//as it says
+    int elapsedTimeInSeconds; // stores total session time
+    QVector<float> heartRateData; // stores the list of reported HR data every second
+    QVector<float> coherences; // stores list of coherence data every 5-seconds
+    float averageCoherence;
+    int lowTime, medTime, highTime; // time in seconds, at each coherence level
+    float achievementScore;
+    float currentCoherence;
     QString date;
+    
 };
 
 #endif // SESSION_H
