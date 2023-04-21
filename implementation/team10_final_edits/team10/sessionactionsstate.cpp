d2d326a752c277509701104f7a398d613cfe0c5e
mariamartiyanova <122643825+mariamartiyanova@users.noreply.github.com>
2023-04-20 20:49:27 -0400

    Update sessionactionsstate.cpp

--- implementation/team10_final_edits/team10/sessionactionsstate.cpp
+++ implementation/team10_final_edits/team10/sessionactionsstate.cpp
@@ -1,17 +1,8 @@
 #include "sessionactionsstate.h"
 
-SessionActionsState::SessionActionsState(QString items, QString title) : State(items, title)
-{
+SessionActionsState::SessionActionsState(QString items, QString title) : State(items, title) {}
 
-}
-
-void SessionActionsState::printSelection(QString selectedItem){
-    qInfo() << "you selected = " << selectedItem;
-}
-//state button press returns
 int SessionActionsState::handlePressPowerButton(){return -1;}
 int SessionActionsState::handlePressMenuButton(){return -1;}
 int SessionActionsState::handlePressBackButton(){return -1;}
-int SessionActionsState::handlePressLeftButton(){return -1;}
-int SessionActionsState::handlePressRightButton(){return -1;}
 int SessionActionsState::handlePressSelectorButton(){return -1;}
