ce1a554f9550b1da025b68cb8ae69b9f8bdc3413
mariamartiyanova <122643825+mariamartiyanova@users.noreply.github.com>
2023-04-20 21:04:23 -0400

    Update mainmenustate.h

--- implementation/team10_final_edits/team10/mainmenustate.h
+++ implementation/team10_final_edits/team10/mainmenustate.h
@@ -4,21 +4,18 @@
 #include <QString>
 #include <QDebug>
 #include "state.h"
-class MainMenuState: public State
-{
-public:
-    MainMenuState(QString items, QString title);
-    void printSelection(QString selectedItem);
 
-    //state button press returns
+class MainMenuState: public State{
+    
+public:
+    
+    MainMenuState(QString items, QString title);
+
     int handlePressPowerButton();
     int handlePressMenuButton();
     int handlePressBackButton();
-    int handlePressLeftButton();
-    int handlePressRightButton();
     int handlePressSelectorButton();
 
-private:
 };
 
 #endif // MAINMENUSTATE_H
