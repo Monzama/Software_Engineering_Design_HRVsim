5965c24f539078e12cc0cc181bae5f3735259a78
mariamartiyanova <122643825+mariamartiyanova@users.noreply.github.com>
2023-04-20 20:34:49 -0400

    Update sessionsummarystate.cpp

--- implementation/team10_final_edits/team10/sessionsummarystate.cpp
+++ implementation/team10_final_edits/team10/sessionsummarystate.cpp
@@ -1,7 +1,6 @@
 #include "sessionsummarystate.h"
 
-SessionSummaryState::SessionSummaryState(QString items, QString title) : State(items, title)
-{
+SessionSummaryState::SessionSummaryState(QString items, QString title) : State(items, title){
     deleteOption = false;
     currentIndex = -1;
 }
@@ -12,6 +11,7 @@ int SessionSummaryState::handlePressBackButton(){
     if (deleteOption){return 3;}
     return 2;
 }
+
 int SessionSummaryState::handlePressSelectorButton(){
     if (deleteOption){return 5;}
     return 4;
