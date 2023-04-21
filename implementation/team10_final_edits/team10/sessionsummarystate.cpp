787213f923dea144cf02a01355489164897d8aa1
mariamartiyanova <122643825+mariamartiyanova@users.noreply.github.com>
2023-04-20 20:24:43 -0400

    Update sessionsummarystate.cpp

--- implementation/team10_final_edits/team10/sessionsummarystate.cpp
+++ implementation/team10_final_edits/team10/sessionsummarystate.cpp
@@ -6,19 +6,12 @@ SessionSummaryState::SessionSummaryState(QString items, QString title) : State(i
     currentIndex = -1;
 }
 
-void SessionSummaryState::printSelection(QString selectedItem){
-    qInfo() << "you selected = " << selectedItem;
-}
-
-//state button press returns
 int SessionSummaryState::handlePressPowerButton(){return 1;}
 int SessionSummaryState::handlePressMenuButton(){return 1;}
 int SessionSummaryState::handlePressBackButton(){
     if (deleteOption){return 3;}
     return 2;
 }
-int SessionSummaryState::handlePressLeftButton(){return -1;}
-int SessionSummaryState::handlePressRightButton(){return -1;}
 int SessionSummaryState::handlePressSelectorButton(){
     if (deleteOption){return 5;}
     return 4;
